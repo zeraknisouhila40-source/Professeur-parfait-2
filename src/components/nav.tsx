688848ab5d445eb9a0927e4ef3a1a6be7ca9decf
Zerakni Souhila <zeraknisouhila40@gmail.com>
2025-09-18 19:44:10 +0000

    Try fixing this error: `Console Error: React does not recognize the `asC

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -30,15 +30,16 @@ export function Nav() {
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.href}>
-          <Link href={item.href} asChild>
-            <SidebarMenuButton
-              isActive={pathname === item.href}
-              tooltip={item.label}
-            >
+          <SidebarMenuButton
+            asChild
+            isActive={pathname === item.href}
+            tooltip={item.label}
+          >
+            <Link href={item.href}>
               <item.icon />
               <span>{item.label}</span>
-            </SidebarMenuButton>
-          </Link>
+            </Link>
+          </SidebarMenuButton>
         </SidebarMenuItem>
       ))}
     </SidebarMenu>
